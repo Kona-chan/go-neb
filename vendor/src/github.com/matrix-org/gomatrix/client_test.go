3db43c2c8291c96514563c6804377dfe886931e1
Kona-chan <kona-chan@eureka7.ru>
2017-08-07 23:12:25 +0300

    Update gomatrix to the latest version

--- vendor/src/github.com/matrix-org/gomatrix/client_test.go
+++ vendor/src/github.com/matrix-org/gomatrix/client_test.go
@@ -1,28 +1,105 @@
 package gomatrix
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
 
-func ExampleClient_BuildURLWithQuery() {
-	cli, _ := NewClient("https://matrix.org", "@example:matrix.org", "abcdef123456")
-	out := cli.BuildURLWithQuery([]string{"sync"}, map[string]string{
-		"filter_id": "5",
+func TestClient_LeaveRoom(t *testing.T) {
+	cli := mockClient(func(req *http.Request) (*http.Response, error) {
+		if req.Method == "POST" && req.URL.Path == "/_matrix/client/r0/rooms/!foo:bar/leave" {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(`{}`)),
+			}, nil
+		}
+		return nil, fmt.Errorf("unhandled URL: %s", req.URL.Path)
 	})
-	fmt.Println(out)
-	// Output: https://matrix.org/_matrix/client/r0/sync?access_token=abcdef123456&filter_id=5
+
+	if _, err := cli.LeaveRoom("!foo:bar"); err != nil {
+		t.Fatalf("LeaveRoom: error, got %s", err.Error())
+	}
 }
 
-func ExampleClient_BuildURL() {
-	userID := "@example:matrix.org"
-	cli, _ := NewClient("https://matrix.org", userID, "abcdef123456")
-	out := cli.BuildURL("user", userID, "filter")
-	fmt.Println(out)
-	// Output: https://matrix.org/_matrix/client/r0/user/@example:matrix.org/filter?access_token=abcdef123456
+func TestClient_GetAvatarUrl(t *testing.T) {
+	cli := mockClient(func(req *http.Request) (*http.Response, error) {
+		if req.Method == "GET" && req.URL.Path == "/_matrix/client/r0/profile/@user:test.gomatrix.org/avatar_url" {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(`{"avatar_url":"mxc://matrix.org/iJaUjkshgdfsdkjfn"}`)),
+			}, nil
+		}
+		return nil, fmt.Errorf("unhandled URL: %s", req.URL.Path)
+	})
+
+	if response, err := cli.GetAvatarURL(); err != nil {
+		t.Fatalf("GetAvatarURL: Got error: %s", err.Error())
+	} else if response == "" {
+		t.Fatal("GetAvatarURL: Got empty response")
+	} else if response != "mxc://matrix.org/iJaUjkshgdfsdkjfn" {
+		t.Fatalf("Unexpected response URL: %s", response)
+	}
+
 }
 
-func ExampleClient_BuildBaseURL() {
-	userID := "@example:matrix.org"
-	cli, _ := NewClient("https://matrix.org", userID, "abcdef123456")
-	out := cli.BuildBaseURL("_matrix", "client", "r0", "directory", "room", "#matrix:matrix.org")
-	fmt.Println(out)
-	// Output: https://matrix.org/_matrix/client/r0/directory/room/%23matrix:matrix.org?access_token=abcdef123456
+func TestClient_SetAvatarUrl(t *testing.T) {
+	cli := mockClient(func(req *http.Request) (*http.Response, error) {
+		if req.Method == "PUT" && req.URL.Path == "/_matrix/client/r0/profile/@user:test.gomatrix.org/avatar_url" {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(`{}`)),
+			}, nil
+		}
+		return nil, fmt.Errorf("unhandled URL: %s", req.URL.Path)
+	})
+
+	if err := cli.SetAvatarURL("https://foo.com/bar.png"); err != nil {
+		t.Fatalf("GetAvatarURL: Got error: %s", err.Error())
+	}
+}
+
+func TestClient_StateEvent(t *testing.T) {
+	cli := mockClient(func(req *http.Request) (*http.Response, error) {
+		if req.Method == "GET" && req.URL.Path == "/_matrix/client/r0/rooms/!foo:bar/state/m.room.name" {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBufferString(`{"name":"Room Name Goes Here"}`)),
+			}, nil
+		}
+		return nil, fmt.Errorf("unhandled URL: %s", req.URL.Path)
+	})
+
+	content := struct {
+		Name string `json:"name"`
+	}{}
+
+	if err := cli.StateEvent("!foo:bar", "m.room.name", "", &content); err != nil {
+		t.Fatalf("StateEvent: error, got %s", err.Error())
+	}
+	if content.Name != "Room Name Goes Here" {
+		t.Fatalf("StateEvent: got %s, want %s", content.Name, "Room Name Goes Here")
+	}
+}
+
+func mockClient(fn func(*http.Request) (*http.Response, error)) *Client {
+	mrt := MockRoundTripper{
+		RT: fn,
+	}
+
+	cli, _ := NewClient("https://test.gomatrix.org", "@user:test.gomatrix.org", "abcdef")
+	cli.Client = &http.Client{
+		Transport: mrt,
+	}
+	return cli
+}
+
+type MockRoundTripper struct {
+	RT func(*http.Request) (*http.Response, error)
+}
+
+func (t MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	return t.RT(req)
 }
